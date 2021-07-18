a47a9c817d2f36b0933b71a39bfbb1b35f108b7e
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-07-18 11:10:25 -0900

    Update make_v.sh

--- make_v.sh
+++ make_v.sh
@@ -2,7 +2,7 @@ git clone --depth 1 --quiet --single-branch https://github.com/vlang/v vrep
 cd vrep 
 make latest_tcc
 cd ..
-git clone --depth 1 --quiet --single-branchhttps://github.com/vlang/vc v
+git clone --depth 1 --quiet --single-branch https://github.com/vlang/vc v
 cd v
 gcc -o v v.c
 cd ..
