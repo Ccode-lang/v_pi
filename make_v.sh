0dae16243ba5785bce7dd9acb97125725818c186
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-07-19 08:35:30 -0900

    make build look better

--- make_v.sh
+++ make_v.sh
@@ -1,14 +1,19 @@
+printf "fetching tcc...\n\n"
 git clone --depth 1 --quiet --single-branch https://github.com/vlang/v vrep
 cd vrep 
 make latest_tcc
 cd ..
+printf "fetching vc...\n\n"
 git clone --depth 1 --quiet --single-branch https://github.com/vlang/vc v
 cd v
-gcc -o v v.c
+printf "building v bianary...\n\n"
+gcc -o v v.c > /dev/null
 cd ..
+printf "copying extras...\n\n"
 cp -r vrep/vlib v/
 cp -r vrep/thirdparty v/
 cp -r vrep/cmd v/
+printf "removing v repo...\n\n"
 rm -rf vrep
 printf "\n\n\n\nopening v repl\n\n\n\n"
 v/v
